b4c5c2c00b666ba0eccb961d4cf13823fb142dec
Bruno Carvalho <bgc1988@gmail.com>
2025-10-17 21:48:21 -0300

    ajuses tlayou

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -12,7 +13,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    
+    <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
@@ -24,7 +25,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-  
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
